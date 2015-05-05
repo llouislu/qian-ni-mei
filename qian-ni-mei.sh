d33c8763a4dc2d99f4841817f7971b595fba7206
larsenlouis <saytolive@live.cn>
2015-05-05 16:02:57 +0800

    better output and introduced error simple handling

--- qian-ni-mei.sh
+++ qian-ni-mei.sh
@@ -28,30 +28,83 @@ password=""
 # ----------------------------------------------
 
 # ---!!!!DO NOT MODIFY THE FOLLOWING LINES!!!!---
+
+# Json query function
+# Credit cjus https://gist.github.com/cjus/1047794
+# Adaptation larsenlouis
+function jsonq() {
+	# argument 1: json input;
+	# argument 2: query;
+	[[ $# != 2 ]] && echo "bad argument(s)." && exit 2
+	# $1 is input of json
+	# $2 is query
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $2`
+    echo $(echo ${temp##*|} |cut -d':' -f2)
+    #original output
+    #echo ${temp##*|}
+}
+
+# example:
+# json = '{"status":1,"info":"","data":false}'
+# jsonq $json "status"|"info"|"data"
+# output "1" | "" | "false"
+
+function err_handle() {
+	[[ $# != 1 ]] && echo "bad argument(s)." && exit 2
+	# $1 is timestamp;
+	rm -rf session-$timestamp.txt
+	rm -rf usercookie-$timestamp.txt
+	echo "Terminated with error(s)!"
+}
+
+#while true; do
+
 timestamp=$(date +%s)
+err=0
 
 # get php session
-
-curl "http://www.zimuzu.tv/user/login/" -c session-$timestamp.txt
+echo "Requesting login page...."
+curl -s "http://www.zimuzu.tv/user/login/" -c session-$timestamp.txt > /dev/null
 
 sleep 1s
 
 # post login info with the session and get user cookie
-
-curl "http://www.zimuzu.tv/user/login/ajaxlogin" -b session-$timestamp.txt --data "account=$user&password=$password&remember=0&back_url=" -c usercookie-$timestamp.txt
-
+echo "Sending your account confidential..."
+echo "Server response to your login info: 0=failed, 1=succeeded"
+json=$(curl -s "http://www.zimuzu.tv/user/login/ajaxlogin" -b session-$timestamp.txt --data "account=$user&password=$password&remember=0&back_url=" -c usercookie-$timestamp.txt)
+result=$(jsonq $json "status")
+echo $result
+if [ "$result" == "0" ]; then
+	echo "Failed to login. Please check your account confidential!" && let err=err+1 && err_handle $timestamp && exit $err
+fi
+echo "Please wait for 5 seconds as page-rediect simulation..."
 sleep 5s
 
 # goto user sign page
-
-curl "http://www.zimuzu.tv/user/sign" -b usercookie-$timestamp.txt
-
+echo "Requesting user home page..."
+curl -s "http://www.zimuzu.tv/user/sign" -b usercookie-$timestamp.txt > /dev/null
+echo "Please wait for 20 seconds as js-button-timer simulation..."
 sleep 20s
 
 # sign with the user cookie
-
-curl "http://www.zimuzu.tv/user/sign/dosign" -b usercookie-$timestamp.txt
+echo "Sending your sign request..."
+echo "Server response to your sign: 0=failed, 1=successed"
+json=$(curl -s "http://www.zimuzu.tv/user/sign/dosign" -b usercookie-$timestamp.txt)
+result=$(jsonq $json "status")
+echo $result
+if [ "$result" == "0" ]; then
+	echo "Failed to sign. Oh, God forbid you to do this!" && let err=err+1 && err_handle $timestamp && exit $err
+fi
 
 # clean up cookies
+echo "Cleaning up cookies..."
 rm -rf session-$timestamp.txt
 rm -rf usercookie-$timestamp.txt
+
+echo "Done!"
+
+# loop in 24 hrs
+
+#sleep 24h
+
+#done
